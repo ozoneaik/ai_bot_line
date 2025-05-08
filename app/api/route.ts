e09fde66a261bbc6de98d5ffbb3dc1523c0e398e
phuwadech <aofphuwadech@gmail.com>
2025-05-08 11:51:49 +0700

    change status

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,5 +2,5 @@ import { logLocal } from "@/utils/Logger";
 
 export async function GET(){
     logLocal({message : 'hello'});
-    return Response.json({message : 'GET request received at /api/route'},{status : 400});
+    return Response.json({message : 'GET request received at /api/route'},{status : 200});
 }
